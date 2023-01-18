c072b8f73ef98572af64852148a9d7e8b866aaf8
Tyagi <utsav.tyagi@capgemini.com>
2023-01-18 16:38:32 +0530

    till vid 72

--- learn-spring-boot/src/main/resources/data.sql
+++ learn-spring-boot/src/main/resources/data.sql
@@ -1,2 +1,4 @@
 insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
 values(1001, 'user1', 'cloud aws', CURRENT_DATE(), FALSE );
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(101, 'user1', 'cloud aws', CURRENT_DATE(), FALSE );
