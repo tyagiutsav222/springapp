397d53815686f6c42cc34108aadc3164d04a0a44
Tyagi <utsav.tyagi@capgemini.com>
2023-01-23 13:16:09 +0530

    connected todo app with mysql database

--- learn-spring-boot/src/main/java/com/example/learnspringboot/myfirstwebapp/todo/Todo.java
+++ learn-spring-boot/src/main/java/com/example/learnspringboot/myfirstwebapp/todo/Todo.java
@@ -3,10 +3,13 @@ package com.example.learnspringboot.myfirstwebapp.todo;
 import java.time.LocalDate;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
 
 @Entity
+@Table(name="TodoTable")
 public class Todo {
 	
 	public Todo()
